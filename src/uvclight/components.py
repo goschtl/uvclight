383bdcbe7ecd43bd685b54612c63f81c6767316e
Souheil CHELFOUH <trollfot@gmail.com>
2014-05-15 15:56:05 +0200

    Added the translate method to the namespace, to allow zope i18n translations

--- src/uvclight/components.py
+++ src/uvclight/components.py
@@ -36,6 +36,7 @@ from z3c.table.column import Column, GetAttrColumn, LinkColumn
 from z3c.table.table import Table as BaseTable
 
 import zope.lifecycleevent
+from zope.i18n import translate
 from zope.component import getMultiAdapter, getAdapters
 from zope.event import notify
 from zope.interface import Interface
@@ -55,6 +56,11 @@ class Layout(BaseLayout):
     context(Interface)
     responseFactory = Response
 
+    def namespace(self, **kwargs):
+        namespace = super(Layout, self).namespace(**kwargs)
+        namespace['translate'] = translate
+        return namespace
+    
 
 class View(BaseView):
     baseclass()
@@ -63,6 +69,11 @@ class View(BaseView):
 
     def url(self, obj, name=None, data=None):
         return compute_url(self.request, obj, name, data)
+
+    def namespace(self):
+        namespace = super(View, self).namespace()
+        namespace['translate'] = translate
+        return namespace
 
     def application_url(self):
         return self.request.application_url
@@ -141,6 +152,7 @@ class Form(BaseForm):
     def namespace(self):
         namespace = super(Form, self).namespace()
         namespace['macro'] = self.getTemplate().macros
+        namespace['translate'] = translate
         return namespace
 
     def redirect(self, url, code=302):
