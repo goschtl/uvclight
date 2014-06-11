daa7572f5c6e08f131a5de5344aa9a4b1e47517d
goschtl <ck@novareto.de>
2014-06-11 11:13:33 +0200

    add new extension

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,6 +35,7 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.viewcode',
     'sphinxcontrib.zopeext.autointerface',
+    'horae.sphinx.grok',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
