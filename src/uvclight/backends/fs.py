a7ded5d88d150368414185a56885a858f0e427be
Souheil CHELFOUH <trollfot@gmail.com>
2014-09-11 21:44:03 +0200

    Improved mongo capabilities. Added FS based manipulation utilities

---
+++ src/uvclight/backends/fs.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+
+import os
+from cromlech.webob import Response
+from mimetypes import guess_type
+
+CHUNK_SIZE = 1 << 12
+
+
+class FileIterable(object):
+
+    def __init__(self, iterator):
+        self.iterator = iterator
+
+    def __iter__(self):
+        return iter(self.iterator)
+
+    @classmethod
+    def make_response(cls, file, filename, content_type, content_length):
+        fileobj = cls(file)
+        res = Response(content_type=fileobj.content_type)
+        res.content_disposition = 'attachment; filename="%s"' % filename
+        res.app_iter = fileobj
+        res.content_length = content_length
+        return res
+
+
+class FileIterator(object):
+    chunk_size = CHUNK_SIZE
+
+    def __init__(self, f):
+        self.fp = f
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        chunk = self.fp.read(self.chunk_size)
+        if not chunk:
+            self.fp.close()
+            raise StopIteration
+        return chunk
+
+
+class Folder(Location):
+
+    def __init__(self, path):
+        assert os.path.isdir(path)
+        self.root_path = path
+
+    def get_file(self, name):
+        path = join(self.root_path, name)
+        assert os.path.isfile(path)
+        iterator = FileIterator(open(path, 'rb'))
+        size = os.path.getsize(path)
+        return name, iterator, size
+
+    def keys(self):
+        return os.listdir(self.root_path)
+    
+    def __getitem__(self, name):
+        filename, data, size = self.get_file(name)
+        type, encoding = guess_type(filename)
+        return FileIterable.make_response(data, name, type, size)
--- src/uvclight/backends/mongo.py
+++ src/uvclight/backends/mongo.py
@@ -1,8 +1,81 @@
 try:
-    import pymongo
+    from pymongo import MongoClient
+    from cromlech.browser.interfaces import IPublicationRoot
+    from zope.interface import implementer
+    from zope.location import Location
 
-    # pyflakes bypass : work in progress
-    pymongo
+
+    class Collection(object):
+        """Abstraction object to represent and handle Mongo Collections.
+        """
+        
+        def __init__(self, manager, path):
+            self.path = path
+            self.manager = manager
+
+        def get_docs(self, *filters):
+            results = self.manager(*filters)
+            return map(doc_marshaller, result)
+
+
+    class Container(Location):
+
+        def __init__(self, collection):
+            self.collection = collection
+        
+        def __getitem__(self, name):
+            return self.collection.get(name)
+
+
+    @implementer(IPublicationRoot)
+    class Root(Location, dict):
+
+        def __init__(self, session):
+            self.session = session
+
+        def add(self, collection, ):
+            
+
+    def connect(dsn=None, auto_start_request=False):
+        if dsn is not None:
+            return MongoClient(dsn, auto_start_request=auto_start_request)
+        return MongoClient(auto_start_request=auto_start_request)
+
+
+    class MongoDBIndexer(object):
+
+        def __init__(self, client, collection):
+            self.client = client
+            self.collection = collection
+
+        def indexDoc(self, *datas):
+            with self.client.start_request():
+                self.collection.insert(*datas)
+            self.collection.ensure_index("state")
+
+        def reindexDoc(self, *datas):
+            with self.client.start_request():
+                for data in datas:
+                    self.collection.save(data)
+
+        def unindexDoc(self, *datas):
+            with self.client.start_request():
+                for data in datas:
+                    self.collection.remove(data, True)
+
+        def listDocs(self, *args, **kwargs):
+            """Override this method to work on filters.
+            """
+            filters = {}
+            with self.client.start_request():
+                docs = self.collection.find(**filters)
+            return list(docs)
+
+        def purge(self):
+            """Use with care.
+            """
+            self.collection.remove()
+
 
 except ImportError:
     print "MongoDB capabilities don't seem to be activated"
